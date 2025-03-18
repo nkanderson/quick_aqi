48c65d8c2c905d8bca6a1f8abbd9ac31fe96ac06
Niklas Anderson <nkanders@gmail.com>
2025-03-18 12:47:37 -0700

    Split out AQI functionality into a separate module

---
+++ src/aqi.rs
@@ -0,0 +1,115 @@
+//! AQI module
+//!
+//! This module provides supporting functionality for AQI calculations and
+//! translations to EPA specified AQI color ranges.
+
+/// Color enum provides colors corresponding to EPA AQI levels
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Color {
+    Green,
+    Yellow,
+    Orange,
+    Red,
+    Purple,
+    DarkPurple,
+}
+
+/// Calulate the AQI for the provided PM2.5 value.
+///
+/// # Arguments
+///
+/// * `pm25` - The PM 2.5 value from the sensor
+///
+/// # Returns
+///
+/// The calculated AQI value using breakpoints and a formula
+/// provided by the EPA. These values may be confirmed using
+/// the calculator at https://www.airnow.gov/aqi/aqi-calculator-concentration/
+///
+/// # Examples
+///
+/// ```
+/// let pm25_concentration = 41;
+/// let aqi = calculate_aqi(pm25_concentration as f32);
+/// assert_eq!(115, aqi);
+///
+/// let pm25_concentration = 7;
+/// let aqi = calculate_aqi(pm25_concentration as f32);
+/// assert_eq!(39, aqi);
+/// ```
+pub fn calculate_aqi(pm25: f32) -> u16 {
+    // AQI breakpoints for PM2.5
+    // Updated in 2024, see the following from the EPA:
+    // https://www.epa.gov/system/files/documents/2024-02/pm-naaqs-air-quality-index-fact-sheet.pdf
+    // https://document.airnow.gov/technical-assistance-document-for-the-reporting-of-daily-air-quailty.pdf
+    const PM25_BREAKPOINTS: [(f32, f32); 6] = [
+        (0.0, 9.0),     // Good
+        (9.1, 35.4),    // Moderate
+        (35.5, 55.4),   // Unhealthy for Sensitive Groups
+        (55.5, 125.4),  // Unhealthy
+        (125.5, 225.4), // Very Unhealthy
+        (225.5, 500.0), // Hazardous
+    ];
+
+    // AQI values corresponding to breakpoints
+    const AQI_BREAKPOINTS: [(u16, u16); 6] = [
+        (0, 50),    // Good
+        (51, 100),  // Moderate
+        (101, 150), // Unhealthy for Sensitive Groups
+        (151, 200), // Unhealthy
+        (201, 300), // Very Unhealthy
+        (301, 500), // Hazardous
+    ];
+
+    // Find the appropriate breakpoint range
+    for i in 0..PM25_BREAKPOINTS.len() {
+        let (pm_low, pm_high) = PM25_BREAKPOINTS[i];
+        if pm25 >= pm_low && pm25 <= pm_high {
+            let (aqi_low, aqi_high) = AQI_BREAKPOINTS[i];
+
+            // Linear interpolation formula transcribed from EPA documentation
+            // AQI = ((AQIhigh - AQIlow) / (PMhigh - PMlow)) * (PMactual - PMlow) + AQIlow
+            let aqi = ((aqi_high - aqi_low) as f32 / (pm_high - pm_low)) * (pm25 - pm_low)
+                + aqi_low as f32;
+            return libm::roundf(aqi) as u16;
+        }
+    }
+
+    // If PM2.5 is above 500, return the maximum AQI value
+    500
+}
+
+/// Provides a Color enum variant value based on the
+/// specified AQI value. Uses the ranges provided by the
+/// EPA for mapping AQI to color.
+///
+/// # Arguments
+///
+/// * `aqi` - The calculated AQI
+///
+/// # Returns
+///
+/// A Color enum variant.
+///
+/// # Examples
+///
+/// ```
+/// let data = parse_data(&sensor_data).unwrap_or_else(|err| {
+///     hprintln!("Error parsing data: {}", err);
+///     Pmsa003iData::default()
+/// });
+///
+/// let pm25_concentration = data.pm2_5_env;
+/// let aqi = calculate_aqi(pm25_concentration as f32);
+/// let color = get_aqi_color(aqi);
+/// ```
+pub fn get_aqi_color(aqi: u16) -> Color {
+    match aqi {
+        0..=50 => Color::Green,
+        51..=100 => Color::Yellow,
+        101..=150 => Color::Orange,
+        151..=200 => Color::Red,
+        201..=300 => Color::Purple,
+        _ => Color::DarkPurple,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,9 @@
 #![no_std]
 #![no_main]
 
+mod aqi;
+
+use crate::aqi::Color;
 use cortex_m_semihosting::hprintln;
 use embassy_executor::Spawner;
 use embassy_stm32::bind_interrupts;
@@ -70,17 +73,6 @@ pub struct Pmsa003iData {
     _particles_2_5: u16, // Number of particles with diameter beyond 2.5 um in 0.1L of air
     _particles_5_0: u16, // Number of particles with diameter beyond 5.0 um in 0.1L of air
     _particles_10: u16,  // Number of particles with diameter beyond 10 um in 0.1L of air
-}
-
-/// Color enum provides colors corresponding to EPA AQI levels
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum Color {
-    Green,
-    Yellow,
-    Orange,
-    Red,
-    Purple,
-    DarkPurple,
 }
 
 /// The LedController struct maps human-readable LED
@@ -270,13 +262,13 @@ async fn main(_spawner: Spawner) {
                     let pm25_concentration = data.pm2_5_env;
 
                     // Convert concentration to AQI
-                    aqi = calculate_aqi(pm25_concentration as f32);
+                    aqi = aqi::calculate_aqi(pm25_concentration as f32);
                     hprintln!("PM2.5 concentration: {} µg/m³", pm25_concentration);
                 }
                 Err(e) => hprintln!("Error reading registers: {:?}", e),
             }
             // Get color name from AQI value
-            let color = get_aqi_color(aqi);
+            let color = aqi::get_aqi_color(aqi);
 
             // Set the LED color
             led_controller.set_color(color);
@@ -288,71 +280,6 @@ async fn main(_spawner: Spawner) {
             led_controller.all_off();
         }
     }
-}
-
-/// Calulate the AQI for the provided PM2.5 value.
-///
-/// # Arguments
-///
-/// * `pm25` - The PM 2.5 value from the sensor
-///
-/// # Returns
-///
-/// The calculated AQI value using breakpoints and a formula
-/// provided by the EPA. These values may be confirmed using
-/// the calculator at https://www.airnow.gov/aqi/aqi-calculator-concentration/
-///
-/// # Examples
-///
-/// ```
-/// let pm25_concentration = 41;
-/// let aqi = calculate_aqi(pm25_concentration as f32);
-/// assert_eq!(115, aqi);
-///
-/// let pm25_concentration = 7;
-/// let aqi = calculate_aqi(pm25_concentration as f32);
-/// assert_eq!(39, aqi);
-/// ```
-fn calculate_aqi(pm25: f32) -> u16 {
-    // AQI breakpoints for PM2.5
-    // Updated in 2024, see the following from the EPA:
-    // https://www.epa.gov/system/files/documents/2024-02/pm-naaqs-air-quality-index-fact-sheet.pdf
-    // https://document.airnow.gov/technical-assistance-document-for-the-reporting-of-daily-air-quailty.pdf
-    const PM25_BREAKPOINTS: [(f32, f32); 6] = [
-        (0.0, 9.0),     // Good
-        (9.1, 35.4),    // Moderate
-        (35.5, 55.4),   // Unhealthy for Sensitive Groups
-        (55.5, 125.4),  // Unhealthy
-        (125.5, 225.4), // Very Unhealthy
-        (225.5, 500.0), // Hazardous
-    ];
-
-    // AQI values corresponding to breakpoints
-    const AQI_BREAKPOINTS: [(u16, u16); 6] = [
-        (0, 50),    // Good
-        (51, 100),  // Moderate
-        (101, 150), // Unhealthy for Sensitive Groups
-        (151, 200), // Unhealthy
-        (201, 300), // Very Unhealthy
-        (301, 500), // Hazardous
-    ];
-
-    // Find the appropriate breakpoint range
-    for i in 0..PM25_BREAKPOINTS.len() {
-        let (pm_low, pm_high) = PM25_BREAKPOINTS[i];
-        if pm25 >= pm_low && pm25 <= pm_high {
-            let (aqi_low, aqi_high) = AQI_BREAKPOINTS[i];
-
-            // Linear interpolation formula transcribed from EPA documentation
-            // AQI = ((AQIhigh - AQIlow) / (PMhigh - PMlow)) * (PMactual - PMlow) + AQIlow
-            let aqi = ((aqi_high - aqi_low) as f32 / (pm_high - pm_low)) * (pm25 - pm_low)
-                + aqi_low as f32;
-            return libm::roundf(aqi) as u16;
-        }
-    }
-
-    // If PM2.5 is above 500, return the maximum AQI value
-    500
 }
 
 /// Parses raw buffer data from the PMSA003I sensor
@@ -549,41 +476,6 @@ fn validate_checksum(checksum_bytes: &[u8]) -> Result<(), &'static str> {
         Ok(())
     } else {
         Err("Checksum validation failed")
-    }
-}
-
-/// Provides a Color enum variant value based on the
-/// specified AQI value. Uses the ranges provided by the
-/// EPA for mapping AQI to color.
-///
-/// # Arguments
-///
-/// * `aqi` - The calculated AQI
-///
-/// # Returns
-///
-/// A Color enum variant.
-///
-/// # Examples
-///
-/// ```
-/// let data = parse_data(&sensor_data).unwrap_or_else(|err| {
-///     hprintln!("Error parsing data: {}", err);
-///     Pmsa003iData::default()
-/// });
-///
-/// let pm25_concentration = data.pm2_5_env;
-/// let aqi = calculate_aqi(pm25_concentration as f32);
-/// let color = get_aqi_color(aqi);
-/// ```
-fn get_aqi_color(aqi: u16) -> Color {
-    match aqi {
-        0..=50 => Color::Green,
-        51..=100 => Color::Yellow,
-        101..=150 => Color::Orange,
-        151..=200 => Color::Red,
-        201..=300 => Color::Purple,
-        _ => Color::DarkPurple,
     }
 }
 
